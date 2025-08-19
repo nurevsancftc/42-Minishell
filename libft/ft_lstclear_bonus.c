4bfae8eb17bd659ee503ff0d82a022b3571ed13a
Aleyna Sena Durmaz <aldurmaz@k1m25s05.42istanbul.com.tr>
2025-08-20 00:15:55 +0300

    libft norm fix

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuciftci <nuciftci@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: aldurmaz <aldurmaz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 19:36:49 by aldurmaz          #+#    #+#             */
-/*   Updated: 2025/08/08 07:22:41 by nuciftci         ###   ########.fr       */
+/*   Updated: 2025/08/20 00:15:22 by aldurmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*current;
 	t_list	*next_node;
 
-	if (!lst || !del) // Koruma: liste veya silme fonksiyonu NULL ise hiçbir şey yapma
-		return;
+	if (!lst || !del)
+		return ;
 	current = *lst;
 	while (current != NULL)
 	{
-		next_node = current->next; // Bir sonraki düğümü kaydet
-		ft_lstdelone(current, del); // Mevcut düğümü ve içeriğini sil
-		current = next_node;        // Kaydedilen bir sonraki düğüme geç
+		next_node = current->next;
+		ft_lstdelone(current, del);
+		current = next_node;
 	}
-	*lst = NULL; // Listenin başını NULL yap, artık boş.
+	*lst = NULL;
 }
