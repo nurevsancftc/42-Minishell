a77c252b2446c43833212ad22d845bccc3c421e1
Aleyna Sena Durmaz <aldurmaz@k1m25s05.42istanbul.com.tr>
2025-08-20 00:08:45 +0300

    expander.h norms are done

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: aldurmaz <aldurmaz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 18:07:07 by aldurmaz          #+#    #+#             */
-/*   Updated: 2025/08/19 22:24:33 by aldurmaz         ###   ########.fr       */
+/*   Updated: 2025/08/20 00:08:19 by aldurmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,16 @@ void	expand_redirections(t_simple_command *cmd, t_shell *shell);
 t_list	*array_to_list(char **array);
 char	**list_to_array(t_list *list_head);
 char	*ft_strunquote(const char *str);
-int	is_originally_quoted(const char *original_word);
-char	*append_expanded_var(char *new_word, const char **word_ptr, t_shell *shell);
+int		is_originally_quoted(const char *original_word);
+
+char	*append_expanded_var(char *new_word, const char **word_ptr,
+			t_shell *shell);
+
 char	*append_char(char *new_word, char c);
-t_list	*process_node(t_list *node, t_list **arg_list, t_list **prev, t_shell *shell);
+
+t_list	*process_node(t_list *node, t_list **arg_list, t_list **prev,
+			t_shell *shell);
+
 void	expand_args_list(t_list **arg_list, t_shell *shell);
 
 #endif
