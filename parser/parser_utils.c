3d73ca3a8bc231596a99bbbe115d503dba084178
Aleyna Sena Durmaz <aldurmaz@k1m25s05.42istanbul.com.tr>
2025-08-19 20:46:38 +0300

    parser norm is fixed

--- parser/parser_utils.c
+++ parser/parser_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nuciftci <nuciftci@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: aldurmaz <aldurmaz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 19:09:45 by aldurmaz          #+#    #+#             */
-/*   Updated: 2025/08/08 08:58:14 by nuciftci         ###   ########.fr       */
+/*   Updated: 2025/08/19 20:46:09 by aldurmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,52 @@
 
 #include "minishell.h"
 
-// Helper to free a 2D array
-void free_args(char **args)
+void	free_args(char **args)
 {
-    int i;
+	int	i;
 
-    if (!args)
-        return;
-    i = 0;
-    while (args[i])
-    {
-        free(args[i]);
-        i++;
-    }
-    free(args);
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
 }
 
-// Helper to free the content of the redirections list (t_redir structs)
-static void free_redir_node(void *content)
+static void	free_redir_node(void *content)
 {
 	t_redir	*redir;
 
 	if (!content)
-		return;
-	
+		return ;
 	redir = (t_redir *)content;
-	
-	// `redir->filename`'i serbest bırak. NULL kontrolü önemli.
 	if (redir->filename)
 		free(redir->filename);
-	
-	// `t_redir` yapısının kendisini serbest bırak.
 	free(redir);
 }
 
-
-// Parser tarafından oluşturulan tüm komut ağacını serbest bırakır.
-void free_cmd_tree(t_command_chain *head)
+void	free_cmd_tree(t_command_chain *head)
 {
-    t_command_chain		*tmp_chain;
+	t_command_chain		*tmp_chain;
 	t_simple_command	*cmd;
 
-    while (head)
-    {
-        tmp_chain = head->next;
-        cmd = head->simple_command;
-        if (cmd)
-        {
-			// ft_free_array gibi genel bir 2D dizi temizleme fonksiyonu kullan.
-            ft_free_array(cmd->args); 
-            
-			// EN ÖNEMLİ DEĞİŞİKLİK:
-			// Yönlendirme listesini, sadece `content`'i temizleyen
-			// `free_redir_content` fonksiyonu ile temizle.
-            if (cmd->redirections)
-                ft_lstclear(&cmd->redirections, free_redir_node);
-            
+	while (head)
+	{
+		tmp_chain = head->next;
+		cmd = head->simple_command;
+		if (cmd)
+		{
+			ft_free_array(cmd->args);
+			if (cmd->redirections)
+				ft_lstclear(&cmd->redirections, free_redir_node);
 			free(cmd);
-        }
-        free(head);
-        head = tmp_chain;
-    }
+		}
+		free(head);
+		head = tmp_chain;
+	}
 }
 
 char	*ft_strunquote(const char *str)
@@ -86,13 +71,22 @@ char	*ft_strunquote(const char *str)
 		return (NULL);
 	len = ft_strlen(str);
 	quote = str[0];
-	// Eğer string en az 2 karakter uzunluğundaysa ve ilk ile son karakter
-	// aynı tırnak karakteriyse...
 	if (len >= 2 && (quote == '\'' || quote == '"') && str[len - 1] == quote)
-	{
-		// Tırnakların içini kopyala.
 		return (ft_substr(str, 1, len - 2));
-	}
-	// Tırnak yoksa veya eşleşmiyorsa, string'in kendisinin bir kopyasını döndür.
 	return (ft_strdup(str));
+}
+
+void	add_cmd_to_chain(t_command_chain **head, t_command_chain *new_node)
+{
+	t_command_chain	*current;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return ;
+	}
+	current = *head;
+	while (current->next)
+		current = current->next;
+	current->next = new_node;
 }
---
+++ parser/redirections.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldurmaz <aldurmaz@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 20:22:41 by aldurmaz          #+#    #+#             */
+/*   Updated: 2025/08/19 20:31:33 by aldurmaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	set_redir_filename(t_redir *redir, char *delimiter)
+{
+	if (redir->type == TOKEN_HEREDOC)
+	{
+		if (delimiter[0] == '\'' || delimiter[0] == '"')
+			redir->expand_in_heredoc = 0;
+		else
+			redir->expand_in_heredoc = 1;
+		redir->filename = ft_strunquote(delimiter);
+	}
+	else
+	{
+		redir->expand_in_heredoc = 0;
+		redir->filename = ft_strdup(delimiter);
+	}
+	if (!redir->filename)
+		return (-1);
+	return (0);
+}
+
+static int	add_redir_to_cmd(t_redir *redir, t_simple_command *cmd)
+{
+	t_list	*new_node;
+
+	new_node = ft_lstnew(redir);
+	if (!new_node)
+	{
+		free(redir->filename);
+		free(redir);
+		return (-1);
+	}
+	ft_lstadd_back(&cmd->redirections, new_node);
+	return (0);
+}
+
+int	handle_redirection(t_token **token_cursor, t_simple_command *cmd)
+{
+	t_redir	*redir;
+	t_token	*redir_token;
+	t_token	*delimiter_token;
+
+	redir_token = *token_cursor;
+	delimiter_token = redir_token->next;
+	redir = ft_calloc(1, sizeof(t_redir));
+	if (!redir)
+		return (-1);
+	redir->type = redir_token->type;
+	if (set_redir_filename(redir, delimiter_token->value) == -1)
+		return (free(redir), -1);
+	if (add_redir_to_cmd(redir, cmd) == -1)
+		return (-1);
+	*token_cursor = delimiter_token->next;
+	return (0);
+}
